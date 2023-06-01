4423f8f0a2d7baec8fd22d2181a534b171d5b563
Sadler, David <david.sadler21@imperial.ac.uk>
2023-06-01 18:12:25 +0000

    Added database updating

--- src/app/api/student/route.js
+++ src/app/api/student/route.js
@@ -4,6 +4,20 @@ import { NextResponse } from 'next/server';
 const prisma = new PrismaClient();
 
 export async function GET() {
-    const student = await prisma.student.findFirst();
-    return NextResponse.json(student);
+  const student = await prisma.student.findFirst();
+  return NextResponse.json(student);
+}
+
+export async function PUT(request) {
+  const { language, maths } = await request.json();
+  await prisma.student.update({
+    where: {
+      id: 1
+    },
+    data: {
+      language: language,
+      maths: maths
+    }
+  });
+  return NextResponse.json({});
 }
--- src/app/studentDashboard/page.js
+++ src/app/studentDashboard/page.js
@@ -4,9 +4,26 @@ import Accordion from 'react-bootstrap/Accordion';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function StudentDashboard() {
+  const langRef = useRef();
+  const mathRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('/api/student', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        language : langRef.current.value,
+        maths : mathRef.current.value
+      }),
+    });
+  }
+
   const [student, setStudent] = useState([]);
   useEffect(() => {
     fetch('/api/student')
@@ -28,13 +45,13 @@ function StudentDashboard() {
         </Nav.Item>
       </Nav>
       
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Accordion defaultActiveKey={['0']} alwaysOpen>
           <Accordion.Item eventKey="0">
             <Accordion.Header>Language Skill</Accordion.Header>
             <Accordion.Body>
             <Form.Group className="mb-3" controlId="formGroupSkill1">
-              <Form.Control as="textarea" rows={3} placeholder="Enter your evidence of the skill" defaultValue={student.language}/>
+              <Form.Control as="textarea" rows={3} placeholder="Enter your evidence of the skill" defaultValue={student.language} ref={langRef}/>
             </Form.Group>
             </Accordion.Body>
           </Accordion.Item>
@@ -42,16 +59,15 @@ function StudentDashboard() {
             <Accordion.Header>Profient in Maths</Accordion.Header>
             <Accordion.Body>
               <Form.Group className="mb-3" controlId="formGroupSkill2">
-                <Form.Control as="textarea" rows={3} placeholder="Enter your evidence of the skill" defaultValue={student.maths}/>
+                <Form.Control as="textarea" rows={3} placeholder="Enter your evidence of the skill" defaultValue={student.maths} ref={mathRef}/>
               </Form.Group>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
       </Form>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
     </main>
   );
 }
