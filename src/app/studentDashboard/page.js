1b52175d6a407bd2b45deb0c8f7c63326d5c0ee3
Sadler, David <david.sadler21@imperial.ac.uk>
2023-06-01 17:42:07 +0000

    Added database fetching

--- src/app/studentDashboard/page.js
+++ src/app/studentDashboard/page.js
@@ -1,46 +1,51 @@
 'use client'
-
 import "bootstrap/dist/css/bootstrap.min.css"
 import Accordion from 'react-bootstrap/Accordion';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from 'react';
 
-function studentDashboard() {
+function StudentDashboard() {
+  const [student, setStudent] = useState([]);
+  useEffect(() => {
+    fetch('/api/student')
+      .then((response) => response.json())
+      .then((data) => {setStudent(data)} );
+  }, []);
+
   return (
     <main className="studentDashboard">
       <Nav fill className="justify-content-center" activeKey="/home">
-      <Nav.Item>
-        <h6>3 Requirements Matched </h6>
-      </Nav.Item>
-      <Nav.Item>
-      <h4>Your Application </h4>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-2">Upload CV</Nav.Link>
-      </Nav.Item>
+        <Nav.Item>
+          <h6>3 Requirements Matched </h6>
+        </Nav.Item>
+        <Nav.Item>
+        <h4>Your Application </h4>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Upload CV</Nav.Link>
+        </Nav.Item>
       </Nav>
       
-
-    <Form>
-      <Accordion defaultActiveKey={['0']} alwaysOpen>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Language Skill</Accordion.Header>
-          <Accordion.Body>
-          <Form.Group className="mb-3" controlId="formGroupSkill1">
-          <Form.Control as="textarea" rows={3} placeholder="Enter your evidence of the skill"/>
-          </Form.Group>
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Profient in Maths</Accordion.Header>
-          <Accordion.Body>
-          <Form.Group className="mb-3" controlId="formGroupSkill2">
-            <Form.Label>Password</Form.Label>
-            <Form.Control as="textarea" rows={3} placeholder="Enter your evidence of the skill"/>
-          </Form.Group>
-          </Accordion.Body>
-        </Accordion.Item>
+      <Form>
+        <Accordion defaultActiveKey={['0']} alwaysOpen>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Language Skill</Accordion.Header>
+            <Accordion.Body>
+            <Form.Group className="mb-3" controlId="formGroupSkill1">
+              <Form.Control as="textarea" rows={3} placeholder="Enter your evidence of the skill" defaultValue={student.language}/>
+            </Form.Group>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Profient in Maths</Accordion.Header>
+            <Accordion.Body>
+              <Form.Group className="mb-3" controlId="formGroupSkill2">
+                <Form.Control as="textarea" rows={3} placeholder="Enter your evidence of the skill" defaultValue={student.maths}/>
+              </Form.Group>
+            </Accordion.Body>
+          </Accordion.Item>
         </Accordion>
       </Form>
 
@@ -48,9 +53,7 @@ function studentDashboard() {
         Submit
       </Button>
     </main>
-    
-    
   );
 }
 
-export default studentDashboard;
+export default StudentDashboard;
