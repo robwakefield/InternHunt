24efc3a6d2c82c19bd9ee1baec689483607567bd
David Sadler <djs21@ic.ac.uk>
2023-06-05 12:29:15 +0100

    Added blank recruiterInternship page

---
+++ src/app/recruiterInternship/page.js
@@ -0,0 +1,12 @@
+'use client'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function RecruiterInternship() {
+  return (
+    <main className="recruiterInternship">
+
+    </main>
+  );
+}
+
+export default RecruiterInternship;
