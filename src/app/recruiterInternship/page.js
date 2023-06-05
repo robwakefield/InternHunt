77bcd9f1d8c4b28826639d577757f8583fdddeb6
tyw21 <tyw21@ic.ac.uk>
2023-06-05 13:25:58 +0100

    Added SkillList

--- src/app/recruiterInternship/page.js
+++ src/app/recruiterInternship/page.js
@@ -2,20 +2,42 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import Button from "react-bootstrap/Button";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { Component } from "react";
 
 function RecruiterInternship() {
-  const applicants = [
-    {name: "applicant1"},
-    {name: "applicant2"},
-    {name: "applicant3"}
-  ];
-
   return (
     <main className="recruiterInternship">
       <Button href="./recruiterDashboard">Return to Dashboard</Button>
-      <ListGroup componentClass="ul">{applicants.map((applicant) => (<ListGroupItem>{applicant.name}</ListGroupItem>))}</ListGroup>
+      <ApplicantList />
+      <SkillList />
     </main>
   );
 }
 
 export default RecruiterInternship;
+
+class ApplicantList extends Component {
+  applicants = [
+    {name: "applicant1"},
+    {name: "applicant2"},
+    {name: "applicant3"}
+  ];
+  render() {
+    return (
+      <ListGroup componentClass="ul">{this.applicants.map((applicant) => (<ListGroupItem>{applicant.name}</ListGroupItem>))}</ListGroup>
+    )
+  }
+}
+
+class SkillList extends Component {
+  skills = [
+    {name: "skill1"},
+    {name: "skill2"},
+    {name: "skill3"}
+  ];
+  render() {
+    return (
+      <ListGroup componentClass="ul">{this.skills.map((skill) => (<ListGroupItem>{skill.name}</ListGroupItem>))}</ListGroup>
+    )
+  }
+}
