9c69c6b7394f43f5789940117f38747b62d7eaea
David Sadler <djs21@ic.ac.uk>
2023-06-05 12:55:31 +0100

    Added static applicant list element

--- src/app/recruiterInternship/page.js
+++ src/app/recruiterInternship/page.js
@@ -1,11 +1,19 @@
 'use client'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Button from "react-bootstrap/Button";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 function RecruiterInternship() {
+  const applicants = [
+    {name: "applicant1"},
+    {name: "applicant2"},
+    {name: "applicant3"}
+  ];
+
   return (
     <main className="recruiterInternship">
       <Button href="./recruiterDashboard">Return to Dashboard</Button>
+      <ListGroup componentClass="ul">{applicants.map(({name}) => (<ListGroupItem>{name}</ListGroupItem>))}</ListGroup>
     </main>
   );
 }
