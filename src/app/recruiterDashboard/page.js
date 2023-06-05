aa757a18b789df18b41c6731fb0748b157451224
rob <rgw20@ic.ac.uk>
2023-06-05 15:21:39 +0100

    Add dummy listings

--- src/app/recruiterDashboard/page.js
+++ src/app/recruiterDashboard/page.js
@@ -4,11 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import ListGroup from "react-bootstrap/ListGroup";
-import { Container, Navbar, Card } from "react-bootstrap";
+import { Container, Navbar, Card, ListGroupItem } from "react-bootstrap";
 
 function recruiterDashboard() {
 const handleClick = () => {
-  alert("Going to Intern Page!")
+  window.location.href = "./recruiterInternship";
 }
 
   return (
@@ -28,13 +28,41 @@ const handleClick = () => {
       </Navbar>
       
       {/* Job Listings List */}
-      <Container>
-        <Card>
+      <Container  style={{height: "100px"}}>
+        <Card className="h-100">
           <Card.Header className="d-flex justify-content-between">
             <Button>Sort</Button>
             <h4>My Listings</h4>
             <Button>New Post</Button>
           </Card.Header>
+
+          <ListGroup>
+
+            <ListGroupItem>
+              <Container className="d-flex justify-content-between" onClick={handleClick}>
+                <p className="text-danger">Applications Closed</p>
+                <p className="text-center">IT Intern</p>
+                <p className="text-success">3/50 Applications</p>
+              </Container>
+            </ListGroupItem>
+
+            <ListGroupItem>
+              <Container className="d-flex justify-content-between" onClick={handleClick}>
+                <p className="text-success">Applications Open</p>
+                <p className="text-center">Softare Engineer Intern</p>
+                <p className="text-warning">30/35 Applications</p>
+              </Container>
+            </ListGroupItem>
+
+            <ListGroupItem>
+              <Container className="d-flex justify-content-between" onClick={handleClick}>
+                <p className="text-muted">Draft</p>
+                <p className="text-center">Management Intern</p>
+                <p></p>
+              </Container>
+            </ListGroupItem>
+
+          </ListGroup>
         </Card>
       </Container>
     </main>
