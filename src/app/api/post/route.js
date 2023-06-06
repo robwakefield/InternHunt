6f8c3d2a383557b1aa521f130e9280bad6878c14
David Sadler <djs21@ic.ac.uk>
2023-06-06 11:20:32 +0100

    Improved prisma query in api/post

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -4,17 +4,18 @@ import { NextResponse } from 'next/server';
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const post = await prisma.post.findFirst();
-  post.applications = await prisma.application.findMany({
-    where: {
-      postID: post.id
-    },
-    include: {
-      student: {
+  const post = await prisma.post.findFirst({
+    select: {
+      name: true,
+      applications: {
         select: {
-          name: true
+          student: {
+            select: {
+              name: true
+            }
+          }
         }
-      }
+      },
     }
   });
   return NextResponse.json(post);
