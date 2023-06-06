03c8f74b8ea085ce391af03cc48e420af7e5f670
rob <rgw20@ic.ac.uk>
2023-06-06 18:31:19 +0100

    Instantiate a single instance of PrismaClient

--- src/app/api/listings/route.js
+++ src/app/api/listings/route.js
@@ -1,7 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '../../db/client'
 import { NextResponse } from 'next/server';
-
-const prisma = new PrismaClient();
 
 export async function GET() {
     const listings = await prisma.post.findMany()
