07ee4adf2b8bdb8851b41e81ba7ea3c8bb289f1f
rob <rgw20@ic.ac.uk>
2023-06-06 17:01:00 +0100

    Use post data in recruiter dashboard

--- src/app/recruiterDashboard/page.js
+++ src/app/recruiterDashboard/page.js
@@ -67,7 +67,7 @@ class ApplicantList extends Component {
   render() {
     return (
       <ListGroup>
-        {this.state.listings.map((listing) => <ListingItem key={listing.title}></ListingItem>)}
+        {this.state.listings.map((listing) => <ListingItem key={listing.title} post={listing}></ListingItem>)}
       </ListGroup>
     )
   }
@@ -80,19 +80,45 @@ const handleClick = () => {
 class ListingItem extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      title: "IT Intern",
-      status: "Applications Open",
-      places_filled: "3",
-      total_places: "50"
+    this.state = { 
+      title: this.props.post.name,
+      status: this.props.post.status,
+      places_filled: this.props.post.applications,
+      total_places: this.props.post.totalPlaces
     };
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.listings !== this.props.listings) {
+      this.setState({ 
+        title: this.props.post.name,
+        status: this.props.post.status,
+        places_filled: this.props.post.applications,
+        total_places: this.props.post.totalPlaces
+      });
+    }
+  }
+
   render() {
+    let status_text;
+    switch (this.state.status) {
+      case "Draft":
+        status_text = <p className="text-muted">{this.state.status}</p>
+        break
+      case "Applications Open":
+        status_text = <p className="text-success">{this.state.status}</p>
+        break
+      case "Applications Closed":
+        status_text = <p className="text-error">{this.state.status}</p>
+        break
+      default:
+        status_text = <p>{this.state.status}</p>
+    }
+
     return (
       <ListGroupItem>
         <Container className="d-flex justify-content-between" style={{cursor: "pointer"}} onClick={handleClick}>
-          <p className="text-muted">{this.state.status}</p>
+          {status_text}
           <p className="text-center">{this.state.title}</p>
           <p className="text-warning">
             {this.state.places_filled}/{this.state.total_places} Applications
