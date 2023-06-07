5fd32a420e76af93a8180c1c0d7cfd2da56ed0b5
rob <rgw20@ic.ac.uk>
2023-06-07 17:35:51 +0100

    Remove "Dashboard" link from recruiter navbar

--- src/app/recruiterNavbar.js
+++ src/app/recruiterNavbar.js
@@ -15,11 +15,6 @@ function RecruiterNavbar() {
               className="d-inline-block align-top"
             />{' '}InternHunt</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="./recruiterDashboard">Dashboard</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
       </Container>
     </Navbar>
     )
