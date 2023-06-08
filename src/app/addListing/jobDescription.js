d582242e9c40234b8ba523a8cf6ab894411bd2e5
tyw21 <tyw21@ic.ac.uk>
2023-06-08 21:01:08 +0100

    Added jobDesciption default from database

--- src/app/addListing/jobDescription.js
+++ src/app/addListing/jobDescription.js
@@ -7,28 +7,14 @@ import { Component, useEffect, useRef, useState } from "react";
 import RecruiterNavbar from "../recruiterNavbar";
 
 function JobDescription() {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     listings: props.listings
-  //   };
-  // }
-
-  // componentDidUpdate(prevProps) {
-  //   if (prevProps.listings !== this.props.listings) {
-  //     this.setState({
-  //       listings: this.props.listings
-  //     });
-  //   }
-  // }
-
-  // handleDescriptionChange = (event) => {
-  //   const {onChange} = this.props;
-  //   const description = event.target.value;
-  //   onChange(description);
-  // };
-
   const descRef = useRef();
+  
+  const [description, setDescription] = useState('');
+  useEffect(() => {
+    fetch('/api/listings')
+      .then((response) => response.json())
+      .then((data) => setDescription(data.description));
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -43,22 +29,14 @@ function JobDescription() {
     });
   }
 
-  // const [listings, setListings] = useState([]);
-
-  // useEffect(() => {
-  //   fetch('/api/listings')
-  //     .then((response) => response.json())
-  //     .then((data) => setListings(data));
-  // }, []);
-
   return (
     <Card className="mt-4 mb-2 mx-3">
       <Card.Header className="d-flex justify-content-between">
-      <p>Job Description</p>
-      <Button type='submit'>Edit</Button>
+      <p>Job Description (Autosave ON)</p>
+      <Button>Edit</Button>
       </Card.Header>
       <Form.Group className="mb-3" controlId="formJobDesc">
-        <Form.Control as="textarea" rows={3} placeholder="Enter your Job Description" ref={descRef} onChange={handleSubmit}/>
+        <Form.Control as="textarea" rows={3} placeholder="Enter your Job Description" defaultValue={description} ref={descRef} onChange={handleSubmit}/>
       </Form.Group>
     </Card>
   )
