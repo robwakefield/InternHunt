9cd1697a27cfd46f49fd63319d621fd112e17f63
rob <rgw20@ic.ac.uk>
2023-06-09 12:27:45 +0100

    Add render test to recruiter Dashboard

--- src/app/recruiterDashboard/pageRecruiterDashboard.cy.js
+++ src/app/recruiterDashboard/pageRecruiterDashboard.cy.js
@@ -2,8 +2,14 @@ import React from 'react'
 import RecruiterDashboard from './page'
 
 describe('<RecruiterDashboard />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
+  it('should render and display expected content', () => {
+    // Mount the React component for the page
     cy.mount(<RecruiterDashboard />)
+ 
+    // The new page should contain an h4 with "My Listings"
+    cy.get('h4').contains('My Listings')
+ 
+    // Validate that a card is present
+    cy.get('.card').should('be.visible')
   })
 })
