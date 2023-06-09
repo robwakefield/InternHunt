5000f9503c0eee9dda3229f302e4251eefb04104
rob <rgw20@ic.ac.uk>
2023-06-09 21:39:29 +0100

    Add GET rejection API route

--- src/app/api/reject/route.js
+++ src/app/api/reject/route.js
@@ -1,6 +1,19 @@
 import { prisma } from '../../db/client'
 import { NextResponse } from 'next/server';
 
+export async function GET(request) {
+    const body = await request.json()
+    const student = await prisma.application.findFirst({
+        where: {
+            postID_studentID: {
+                postID: body.postID,
+                studentID: body.studentID
+            }
+        }
+    })
+    return NextResponse.json(student);
+}
+
 export async function PUT(request) {
     const body = await request.json()
     const student = await prisma.application.update({
