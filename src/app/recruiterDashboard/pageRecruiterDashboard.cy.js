235a8dea2dbe1d7e498227c3ff7aabdee0405e01
rob <rgw20@ic.ac.uk>
2023-06-09 12:38:13 +0100

    Add test that recuiter dashboard renders from db correctly

--- src/app/recruiterDashboard/pageRecruiterDashboard.cy.js
+++ src/app/recruiterDashboard/pageRecruiterDashboard.cy.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import RecruiterDashboard from './page'
+import ApplicantList from './page'
 
 describe('<RecruiterDashboard />', () => {
   it('should render and display expected content', () => {
@@ -12,4 +13,30 @@ describe('<RecruiterDashboard />', () => {
     // Validate that a card is present
     cy.get('.card').should('be.visible')
   })
+
+  it('should display listings from DB', () => {
+    // Mount the React component for the page
+    cy.intercept('GET', '/api/listings', {
+      statusCode: 201,
+      body: [{
+        "id":1,
+        "name":"IT Intern",
+        "status":"Applications Open",
+        "totalPlaces":50,
+        "description":"We want an IT Intern!",
+        "rating1Text":"Just Mentioning",
+        "rating2Text":"Minimal Evidence",
+        "rating3Text":"Decent Evidence",
+        "rating4Text":"Excellent Evidence",
+        "rating5Text":"Perfect Match"
+      }]})
+    cy.mount(<RecruiterDashboard />)
+
+    // A lising from the db should be displayed
+    cy.get('.list-group').contains("IT Intern")
+
+    // A listing not in the db should not be displayed
+    cy.get('.list-group').should('not.contain', "Software Engineer")
+  })
+
 })
