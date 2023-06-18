667ffc7ba2b05eb3c533e11a1ed11fd2e1e57bfa
stevenchenwaiho@gmail.com <shc20@ic.ac.uk>
2023-06-18 23:49:40 +0100

    remove duplicate checks in studentDashboard

--- src/app/studentDashboard/page.js
+++ src/app/studentDashboard/page.js
@@ -31,7 +31,7 @@ function StudentDashboard() {
   const cookies = new Cookies();
   const studentId = Number(cookies.get("studentID"));
 
-  if (typeof window !== "undefined" && (typeof window !== "undefined" && (!studentId || isNaN(studentId) || studentId == -1))) {
+  if (typeof window !== "undefined" && (!studentId || isNaN(studentId) || studentId == -1)) {
     window.location.replace("/login");
   }
 
